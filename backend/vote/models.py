02865de2cd913f2fe02118ebdfabbdcf4821af79
dzhfrv <solomonickey@gmail.com>
2020-03-31 23:20:18 +0300

    vote model

--- backend/vote/models.py
+++ backend/vote/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from backend.user.models import User
+from backend.post.models import Post
 
-# Create your models here.
+
+class Vote(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_votes",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.DO_NOTHING,
+        related_name="post_votes",
+    )
+    vote_type = models.CharField()
+
+    class Meta:
+        unique_together = ('user', 'post', 'vote_type')
